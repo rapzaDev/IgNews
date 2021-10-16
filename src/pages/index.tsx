70d95e46766006cd4fdc9a4bd0c7507a9852b510
rapzaDev <rapzadev@gmail.com>
2021-10-16 11:43:23 -0300

    Global styles added

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Start | ig.news </title>
+      </Head>
+      <h1>
+        Hello Tobias and Simba
+      </h1>    
+    </>
+  )
+}
