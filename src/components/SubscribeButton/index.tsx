905ee3780c83520fc776a96ce5a47daf62ae470d
rapzaDev <rapzadev@gmail.com>
2021-10-16 17:24:31 -0300

    Subscribe Button created

---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,14 @@
+import styles from './styles.module.scss';
+
+function SubscribeButton () {
+    return (
+        <button 
+            type="button"    
+            className={styles.subscribeButton}
+        >
+            Subscribe now
+        </button>
+    )
+}
+
+export { SubscribeButton };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,32 @@
 import Head from 'next/head';
 
+import { SubscribeButton } from '../components/SubscribeButton';
+
+import styles from './home.module.scss';
+
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Start | ig.news </title>
+        <title>Home | ig.news </title>
       </Head>
-      <h1>
-        Hello Tobias and Simba
-      </h1>    
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏️ Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+          <SubscribeButton />
+
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl Coding" />
+      </main>
+
     </>
   )
 }
