9a61f20fc83e109c91564e0a8c57f6fbe24af6ab
rapzaDev <rapzadev@gmail.com>
2021-11-17 19:54:39 -0300

    Redirecting to Stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,6 @@
 import { useSession, signIn } from 'next-auth/client';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 
 import styles from './styles.module.scss';
 
@@ -10,13 +12,25 @@ function SubscribeButton ( {priceID}: SubscribeButtonProps) {
 
     const [session] = useSession();
 
-    function handleSubscribe() {
+    async function handleSubscribe() {
         if(!session){
             signIn('github')
             return;
         }
 
-        
+        try {
+            const response = await api.post('/subscribe');
+
+            const { sessionId } = response.data;
+
+            const stripe = await getStripeJs();
+
+
+            await stripe.redirectToCheckout({ sessionId });
+
+        } catch (error) {
+            alert(error.message);
+        }            
         
     }
 
